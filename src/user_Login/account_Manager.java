0212305ccd950bcba49eae595280f1a0f339ca26
YvesSamsonLi <76028406+YvesSamsonLi@users.noreply.github.com>
2023-11-05 12:24:00 +0800

    account_Manager
    linekd account_Manager to user_Registration

--- src/user_Login/account_Manager.java
+++ src/user_Login/account_Manager.java
@@ -22,10 +22,20 @@ public class account_Manager {
             staff_Account staffAccount = new staff_Account(scanner, userId);
             staffAccount.start();
         } else if (userTypeChoice == 1) {
-            System.out.print("Enter your student ID: ");
-            String userId = scanner.next();
-            student_Account studentAccount = new student_Account(scanner, userId);
-            studentAccount.start();
+            System.out.print("Do you have an existing account? (1 for Yes, 0 for No): ");
+            int accountChoice = scanner.nextInt();
+
+            if (accountChoice == 1) {
+                System.out.print("Enter your student ID: ");
+                String userId = scanner.next();
+                student_Account studentAccount = new student_Account(scanner, userId);
+                studentAccount.start();
+            } else if (accountChoice == 0) {
+                // Redirect to the registration process (you need to implement this part)
+                user_Registration.registerNewUser(scanner);
+            } else {
+                System.out.println("Invalid choice. Please enter '1' for Yes or '0' for No.");
+            }
         } else {
             System.out.println("Invalid user type. Please enter '1' for student or '2' for staff.");
         }
