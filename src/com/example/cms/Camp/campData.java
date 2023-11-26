4638d035e8e2f2d1754697237485abeabc56ee73
huaizhic <huaizhihuaizhi@gmail.com>
2023-11-26 14:43:45 +0800

    Javadocs comments for campData class

--- src/com/example/cms/Camp/campData.java
+++ src/com/example/cms/Camp/campData.java
@@ -4,38 +4,38 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * This class represents entity class campData to hold and process data of all camps.
+ * This class represents entity class campData to hold and process data of camps.
  */
 public class campData {
 	private static ArrayList<Camp> campList;
 	private static HashMap<String, Camp> campHashMap= new HashMap<String, Camp>();
 
 	/**
-	 * Gets ArrayList of all Camp objects.
-	 * @return list of all camps.
+	 * Gets ArrayList of Camp objects.
+	 * @return list of camps.
 	 */
 	public static ArrayList<Camp> getCampList() {
 		return campList;
 	}
 
 	/**
-	 * Adds newly created camp object to existing ArrayList of camp objects.
-	 * @param camp
+	 * Adds camp object to existing ArrayList of camp objects.
+	 * @param camp Camp to add
 	 */
 	public static void setCampList(Camp camp) {campList.add(camp);}
 
 	/**
-	 * Adds a key value pair (camp name, camp object) of a newly created camp into the existing hashmap.
-	 * @param name
-	 * @param camp
+	 * Adds a key value pair (camp name, same camp object) of a camp into the existing hashmap.
+	 * @param name name of camp
+	 * @param camp same camp object
 	 */
 	public static void addCampToMap(String name, Camp camp){
 		campHashMap.put(name, camp);
 	}
 
 	/**
-	 * Gets hashmap of all camp names (key) tied to their corresponding camp objects (value)
-	 * @return hashmap of all camp names (key) tied to their corresponding camp objects (value)
+	 * Gets hashmap of camp names (key) tied to their corresponding camp objects (value)
+	 * @return hashmap of camp names (key) tied to their corresponding camp objects (value)
 	 */
 	public static HashMap<String, Camp> getCampHashMap() {
 		return campHashMap;
